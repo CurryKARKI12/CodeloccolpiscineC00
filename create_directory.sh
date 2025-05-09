4dfa044db01a5f506cdf1a4b49a4cae83848dd93
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-09 21:59:00 +0200

    Delete create_directory.sh

--- create_directory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Ce script crée un répertoire nommé test_directory
-# Création du répertoire avec l'option -p pour éviter une erreur si le répertoire existe déjà
-mkdir -p test_directory
-# Message de confirmation
-echo "Le répertoire 'test_directory' a été créé."
