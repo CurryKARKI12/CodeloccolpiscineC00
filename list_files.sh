399c3d5893339ff13941c476f91599050986b190
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-09 21:46:54 +0200

    Delete list_files.sh

--- list_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Ce script affiche la liste de tous les fichiers du répertoire courant
-#ls -p | grep -v /   
-
-echo "Liste des fichiers dans le répertoire courant :"
-# La commande 'find' recherche uniquement les fichiers à la racine du dossier courant
-find . -maxdepth 1 -type f
