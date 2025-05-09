332bb029dcc85a041de4fe2f488a8618c8743960
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-09 22:12:56 +0200

    Add files via upload

---
+++ check_file_exist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Ce script vérifie si un fichier donné existe ou non
+
+# Demande à l'utilisateur d'entrer un nom de fichier
+read -p "Entrez le nom du fichier : " filename
+# Vérifie si le fichier existe et est un fichier régulier
+if [ -f "$filename" ]; then
+    echo "Le fichier '$filename' existe."
+else
+    echo "Le fichier '$filename' n'existe pas."
+fi
