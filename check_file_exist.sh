b1ce9d779c4eb616f9f80e75b18b644cd2f91400
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-10 16:28:06 +0200

    Premier commit

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-# Ce script vérifie si un fichier donné existe ou non.
-# Il demande à l'utilisateur d'entrer un nom de fichier.
-
-read -p "Entrez le nom du fichier : " filename
-
-# Vérifie si un nom a été entré
-if [ -z "$filename" ]; then
-  echo "Erreur : aucun nom de fichier fourni."
-  exit 1
-fi
-
-# Vérifie si le fichier existe
+# Il demande à l'utilisateur d'entrer un nom de fichier
+echo " entrez le nom du fichier à vérifier : "
+read " filename " 
+#Ce script vérifie si un fichier donné existe ou non exite
 if [ -f "$filename" ]; then
   echo "Le fichier '$filename' existe."
 else
