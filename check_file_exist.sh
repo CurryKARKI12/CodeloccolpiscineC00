dcbf836c9537f408fadd70ee295c1a0cbc174fdd
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-10 17:21:28 +0200

    apporter les modifications apportées a check_file_exist

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
-<<<<<<< HEAD
-# Il demande à l'utilisateur d'entrer un nom de fichier
-echo " entrez le nom du fichier à vérifier : "
+# ce script demande à l'utilisateur d'entrer un nom de fichier
+echo " entrez le nom du fichier : " 
 read " filename " 
-#Ce script vérifie si un fichier donné existe ou non exite
-=======
-# Ce script vérifie si un fichier donné existe ou non.
-# Il demande à l'utilisateur d'entrer un nom de fichier.
-
-read -p "Entrez le nom du fichier : " filename
-
-# Vérifie si un nom a été entré
-if [ -z "$filename" ]; then
-  echo "Erreur : aucun nom de fichier fourni."
-  exit 1
-fi
-
-# Vérifie si le fichier existe
->>>>>>> 16f1287987ffd35821c5ec979b89bd0eadbf37fd
+#Ce script vérifie si un fichier donné existe ou non 
 if [ -f "$filename" ]; then
   echo "Le fichier '$filename' existe."
 else
